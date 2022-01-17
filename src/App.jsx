47fd52a18bc7b25069aefb35244a0d792bd528eb
VinSjo <vincentsjogren@gmail.com>
2022-01-17 17:52:50 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './style/app.scss';
 
 function App() {
   // Create the count state.
@@ -12,26 +11,10 @@ function App() {
   }, [count, setCount]);
   // Return the App component.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="app">
+      <p>
+        Page has been open for <code>{count}</code> seconds.
+      </p>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import './index.css';
+import App from './app.jsx';
+import './style/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
