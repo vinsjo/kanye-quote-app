ce7ec0efb1a0087a988e965ca0567f45782c929e
VinSjo <vincentsjogren@gmail.com>
2022-01-19 15:45:36 +0100

    removed unused dependancies and updated readmy

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# New Project
+# Kanye Quotes App
 
-> ✨ Bootstrapped with Create Snowpack App (CSA).
+An app that displays quotes by Kanye West.
+
+Built with: [React](https://reactjs.org/),
+[Snowpack](https://www.snowpack.dev/) and [kanye.rest API](https://kanye.rest/)
 
 ## Available Scripts
 
@@ -9,17 +12,7 @@
 Runs the app in the development mode.
 Open http://localhost:8080 to view it in the browser.
 
-The page will reload if you make edits.
-You will also see any lint errors in the console.
-
 ### npm run build
 
 Builds a static copy of your site to the `build/` folder.
 Your app is ready to be deployed!
-
-**For the best production performance:** Add a build bundler plugin like "@snowpack/plugin-webpack" to your `snowpack.config.mjs` config file.
-
-### npm test
-
-Launches the application test runner.
-Run with the `--watch` flag (`npm test -- --watch`) to run in interactive watch mode.
--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,31 +1,17 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
-  mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
-  },
-  plugins: [
-    '@snowpack/plugin-react-refresh',
-    '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-sass',
-    ['@snowpack/plugin-webpack', {}],
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    bundle: true,
-    minify: true,
-    target: 'es2015',
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+	mount: {
+		public: { url: '/', static: true },
+		src: { url: '/dist' },
+	},
+	plugins: [
+		'@snowpack/plugin-react-refresh',
+		'@snowpack/plugin-dotenv',
+		'@snowpack/plugin-sass',
+	],
+	optimize: {
+		bundle: true,
+		minify: true,
+		target: 'es2018',
+	},
 };
