2cc3e3f5ae559b146bf6e3607236f12723a6f6e1
VinSjo <vincentsjogren@gmail.com>
2022-01-19 18:27:31 +0100

    removed excessive onLoadStart and onLoadEnd callbacks in useFetch and updated styling

--- src/components/KanyeQuotes.jsx
+++ src/components/KanyeQuotes.jsx
@@ -2,45 +2,42 @@ import React, { useState, useEffect } from 'react';
 import useFetch from '../utils/useFetch';
 
 const KanyeQuotes = ({ title }) => {
-  const [loaded, setLoaded] = useState(false);
-  const [outputText, setOutputText] = useState(null);
+	const [outputText, setOutputText] = useState(null);
 
-  const [data, loading, error, reload] = useFetch({
-    url: 'https://api.kanye.rest/',
-    onLoadStart: () => setLoaded(false),
-    onLoadEnd: () => setLoaded(true),
-  });
+	const [data, loading, error, reload] = useFetch({
+		url: 'https://api.kanye.rest/',
+	});
 
-  const updateOutput = () => {
-    setOutputText(
-      loading
-        ? 'loading...'
-        : error
-        ? error
-        : data && data.quote
-        ? data.quote
-        : null,
-    );
-  };
-  useEffect(() => {
-    loaded && updateOutput();
-  }, [data, loading, error, loaded]);
+	const updateOutput = () => {
+		setOutputText(
+			loading
+				? 'loading...'
+				: error
+				? error
+				: data && data.quote
+				? data.quote
+				: null
+		);
+	};
+	useEffect(() => {
+		!loading && updateOutput();
+	}, [data, loading, error]);
 
-  return (
-    <div className="kanye-quotes">
-      <h3 className="title">{title}</h3>
-      <div className={`container${loaded ? ' loaded' : ''}`}>
-        <p>{outputText}</p>
-      </div>
-      <button className="refresh" onClick={reload} disabled={!loaded}>
-        Refresh Quote
-      </button>
-    </div>
-  );
+	return (
+		<div className="kanye-quotes">
+			<h3 className="title">{title}</h3>
+			<div className={`container${loading ? ' loading' : ''}`}>
+				<p>{outputText}</p>
+			</div>
+			<button className="refresh" onClick={reload} disabled={loading}>
+				Refresh Quote
+			</button>
+		</div>
+	);
 };
 
 KanyeQuotes.defaultProps = {
-  title: 'Kanye West Quotes',
+	title: 'Kanye West Quotes',
 };
 
 export default KanyeQuotes;
--- src/utils/useFetch.jsx
+++ src/utils/useFetch.jsx
@@ -1,39 +1,34 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useFetch = ({ url, onLoadStart, onLoadEnd }) => {
-  const [refreshTime, setRefreshTime] = useState(Date.now());
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(null);
-  const [error, setError] = useState(null);
+const useFetch = ({ url }) => {
+	const [refreshTime, setRefreshTime] = useState(Date.now());
+	const [data, setData] = useState(null);
+	const [loading, setLoading] = useState(null);
+	const [error, setError] = useState(null);
 
-  const effectCallback = () => {
-    const source = axios.CancelToken.source();
-    (async () => {
-      try {
-        if (onLoadStart) onLoadStart();
-        setLoading(true);
-        setData(null);
-        setError(null);
-        const response = await axios.get(url, { cancelToken: source.token });
-        setLoading(false);
-        if (response.data) setData(response.data);
-      } catch (err) {
-        setLoading(false);
-        setError(`An error occurred: ${err}`);
-      }
-      if (onLoadEnd) onLoadEnd();
-    })();
-    return () => source.cancel();
-  };
+	const reload = () => setRefreshTime(Date.now());
 
-  useEffect(effectCallback, [url, refreshTime]);
+	useEffect(() => {
+		const source = axios.CancelToken.source();
+		(async () => {
+			try {
+				setLoading(true);
+				setData(null);
+				setError(null);
+				const response = await axios.get(url, {
+					cancelToken: source.token,
+				});
+				if (response.data) setData(response.data);
+			} catch (err) {
+				setError(`An error occurred: ${err}`);
+			}
+			setLoading(false);
+		})();
+		return () => source.cancel();
+	}, [url, refreshTime]);
 
-  const reload = () => {
-    setRefreshTime(Date.now());
-  };
-
-  return [data, loading, error, reload];
+	return [data, loading, error, reload];
 };
 
 export default useFetch;
