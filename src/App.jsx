589b569934f49c572064bf623e108366f8aff73d
VinSjo <vincentsjogren@gmail.com>
2022-01-19 18:54:30 +0100

    added link to github and updated styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import KanyeQuotes from './components/KanyeQuotes';
+import GitLink from './components/GitLink';
 import './style/app.scss';
 
 const App = () => {
-  return (
-    <div className="App">
-      <KanyeQuotes />
-    </div>
-  );
+	return (
+		<div className="App">
+			<KanyeQuotes />
+			<GitLink url="https://github.com/VinSjo/kanye-quote-app" />
+		</div>
+	);
 };
 
 export default App;
